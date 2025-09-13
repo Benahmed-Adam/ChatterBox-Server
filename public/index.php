c5a159851bb544470a1a5e2923681573611d4ecc
Benahmed Adam <adam.benahmeed@gmail.com>
2025-09-13 14:21:03 +0200

    aa

--- public/index.php
+++ public/index.php
@@ -1,10 +1,3 @@
 <?php
 
-use Entity\Collection\GroupCollection;
-use Entity\Collection\UserCollection;
-
-$a = UserCollection::getAllUsers();
-
-foreach ($a as $group) {
-    echo "". $group->getPassword() ."<br>";
-}
+echo "Bienvenue sur ChatterBox";
